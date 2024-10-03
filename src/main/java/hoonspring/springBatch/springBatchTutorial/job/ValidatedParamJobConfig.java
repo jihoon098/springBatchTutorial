fcb79caf42c082c8dae5258fc85da06555b527b1
jihoon098 <jihoon098@naver.com>
2024-10-04 00:33:34 +0900

    배치Job 실행 시, jobParameters를 이용하여 외부 파라미터 전달받기

--- src/main/java/hoonspring/springBatch/springBatchTutorial/job/ValidatedParamJobConfig.java
+++ src/main/java/hoonspring/springBatch/springBatchTutorial/job/ValidatedParamJobConfig.java
@@ -3,42 +3,49 @@ package hoonspring.springBatch.springBatchTutorial.job;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+/*
+ * Description: 스프링배치 실행시 외부로부터 파라미터 전달받기
+ * run: --spring.batch.job.name=validatedParamJob fileName=test.csv
+ */
 @Slf4j
 @Configuration
-public class HelloWorldJobConfig {
+public class ValidatedParamJobConfig {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
 
-    public HelloWorldJobConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public ValidatedParamJobConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         this.jobRepository = jobRepository;
         this.transactionManager = transactionManager;
     }
 
     @Bean
-    public Job helloWorldJob() {
-        // JobBuilder를 사용하여 하나의 작업 단위인 Job 생성
-        return new JobBuilder("helloWorldJob", jobRepository)
-                .incrementer(new RunIdIncrementer()) // 해당 Job이 실행될 때 고유한 ID인 Sequence를 순차적으로 부여
-                .start(helloWorldStep())
+    public Job validatedParamJob(@Qualifier("validatedParamStep") Step validatedParamStep) {
+        return new JobBuilder("validatedParamJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(validatedParamStep)
                 .build();
     }
 
     @Bean
-    public Step helloWorldStep() {
-        // StepBuilder를 사용하여 Job의 작은 실행 단위인 Step을 구성
-        return new StepBuilder("helloWorldStep", jobRepository)
+    @JobScope
+    public Step validatedParamStep(@Value("#{jobParameters['fileName']}") String fileName) {
+        return new StepBuilder("validatedParamStep", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Hello, Spring Batch!");
+                    System.out.println("validated Param Tasklet");
+                    System.out.println("fileName 값은 ? : " + fileName);
                     return RepeatStatus.FINISHED;
                 }, transactionManager)
                 .build();
